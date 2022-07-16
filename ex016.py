aa4b775b61a84e54f584aa4e99e4ba1edf58b96c
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2022-07-16 19:12:32 -0300

    seno, cosseno e tangente

---
+++ ex016.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+from math import radians,sin,cos,tan
+angulo = int(input('Digite o ângulo que você deseja: '))
+seno = sin(radians(angulo))
+print(f'O ângulo de {angulo} tem o SENO de {seno:.2f}')
+cosseno = cos(radians(angulo))
+print (f'O ângulo de {angulo} tem o COSSENO de {cosseno:.2f}')
+tangente = tan(radians(angulo))
+print (f'O ângulo de {angulo} tem a TANGENTE DE {tangente:.2f}')
